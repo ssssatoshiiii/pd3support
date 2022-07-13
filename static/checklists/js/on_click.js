2910603a11bb1457ce14ee5e7665d5e6c918c636
ssssatoshiiii <shimmori-satoshi810@g.ecc.u-tokyo.ac.jp>
2022-07-13 16:16:35 +0900

    ロゴの消去

--- static/checklists/js/on_click.js
+++ static/checklists/js/on_click.js
@@ -66,5 +66,45 @@ function popup(value) {
 }
 
 function test() {
-  alert("yes");
+  alert("yeah");
 }
+
+function detail_action(uri, element){
+ 
+  //それぞれの表のidを取得
+  action_list_number = element.closest('.col-md-10').id.replace('action_list_layer_', '');
+  data = {uri:uri , action_list_number:action_list_number}
+  $.ajax({
+    type: 'POST',
+    url: 'second_graph_list',
+    data: data,
+    dataType: 'text',
+  })
+  .done(function(response){
+    //もしdetail_actionが呼び出されたタグが最下層でなければ、呼び出されたタグの所属する階層以下の階層を消去する
+    for(let i = 0; i < $('#action_list').children('div').length; i++){
+      if(parseInt(action_list_number) < i+1 ){
+        $('#action_list').children('div')[i].remove();
+        i = i -1;
+      }
+    }
+    $('#action_list').append(response);
+  })
+}
+
+
+function show_action_supinfo(action,uri){
+  data = {action:action, uri:uri}
+  $.ajax({
+    type: 'POST',
+    url:'action_supinfo_show',
+    data: data,
+    dataType: 'text',
+  })
+  .done(function(response){
+    $('#action_supinfo').children('div').remove();
+    $('#action_supinfo').append(response);
+  })
+  
+}
+
